5691875e9262772e9126c7af8e28501dd48c5547
foliageSea <1742262457@qq.com>
2025-06-24 10:48:12 +0800

    chore: ServersPage

--- lib/app/features/home/home_page.dart
+++ lib/app/features/home/home_page.dart
@@ -1,13 +1,6 @@
-import 'package:core/core.dart';
-import 'package:file_manger/app/features/home/widgets/actions_dialog.dart';
-import 'package:file_manger/app/features/home/widgets/add_dialog.dart';
-import 'package:file_manger/app/features/home/widgets/files_page.dart';
-import 'package:file_manger/app/utils/theme_color_util.dart';
+import 'package:file_manger/app/features/home/widgets/servers_page.dart';
 import 'package:flutter/material.dart';
-import 'package:file_manger/app/layouts/base_layout.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
-import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import 'home_controller.dart';
 
@@ -22,99 +15,7 @@ class _HomePageState extends State<HomePage> {
   final controller = Get.put(HomeController());
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return BaseLayout(
-      title: 'File Manger',
-      actions: [
-        IconButton(
-          onPressed: () async {
-            await showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return const AddDialog();
-              },
-            );
-          },
-          icon: const Icon(Icons.add),
-        ),
-      ],
-      child: Obx(() => _buildServers()),
-    );
-  }
-
-  Widget _buildServers() {
-    var servers = controller.servers;
-
-    if (servers.isEmpty) {
-      return const Center(child: Icon(LucideIcons.folderMinus));
-    }
-
-    var primaryColor = ThemeColorUtil.getPrimaryColor(context);
-
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: AlignedGridView.count(
-        itemCount: servers.length,
-        crossAxisCount: 4,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        itemBuilder: (context, index) {
-          var server = servers[index];
-          return Ink(
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-            child: InkWell(
-              borderRadius: BorderRadius.circular(8),
-              highlightColor: ThemeColorUtil.getPrimaryColorWithAlpha(context),
-              hoverColor: ThemeColorUtil.getPrimaryColorWithAlpha(context),
-              onTap: () {
-                Get.to(FilesPage(serverModel: server));
-              },
-              onLongPress: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return ActionsDialog(serverModel: server);
-                  },
-                );
-              },
-              child: Container(
-                height: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(
-                    color: ThemeColorUtil.getPrimaryColorWithAlpha(context),
-                  ),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(LucideIcons.server, size: 36, color: primaryColor),
-                    Text(
-                      server.name,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      server.username,
-                      style: const TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ].insertSizedBoxBetween(height: 4),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
+    return const ServersPage();
   }
 }
---
+++ lib/app/features/home/widgets/servers_page.dart
@@ -0,0 +1,110 @@
+import 'package:core/core.dart';
+import 'package:file_manger/app/layouts/base_layout.dart';
+import 'package:file_manger/app/utils/theme_color_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:get/get.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+
+import '../home_controller.dart';
+import 'actions_dialog.dart';
+import 'add_dialog.dart';
+import 'files_page.dart';
+
+class ServersPage extends StatefulWidget {
+  const ServersPage({super.key});
+
+  @override
+  State<ServersPage> createState() => _ServersPageState();
+}
+
+class _ServersPageState extends State<ServersPage> {
+  HomeController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseLayout(
+      title: 'File Manger',
+      actions: [
+        IconButton(
+          onPressed: () async {
+            await showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return const AddDialog();
+              },
+            );
+          },
+          icon: const Icon(Icons.add),
+        ),
+      ],
+      child: Obx(() => _buildServers()),
+    );
+  }
+
+  Widget _buildServers() {
+    var servers = controller.servers;
+
+    if (servers.isEmpty) {
+      return const Center(child: Icon(LucideIcons.folderMinus));
+    }
+
+    var primaryColor = ThemeColorUtil.getPrimaryColor(context);
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: AlignedGridView.count(
+        itemCount: servers.length,
+        crossAxisCount: 4,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        itemBuilder: (context, index) {
+          var server = servers[index];
+          return Ink(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(8),
+              highlightColor: ThemeColorUtil.getPrimaryColorWithAlpha(context),
+              hoverColor: ThemeColorUtil.getPrimaryColorWithAlpha(context),
+              onTap: () {
+                Get.to(FilesPage(serverModel: server));
+              },
+              onLongPress: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return ActionsDialog(serverModel: server);
+                  },
+                );
+              },
+              child: Container(
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color: ThemeColorUtil.getPrimaryColorWithAlpha(context),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(LucideIcons.server, size: 36, color: primaryColor),
+                    Text(
+                      server.name,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      server.username,
+                      style: const TextStyle(fontSize: 12, color: Colors.grey),
+                    ),
+                  ].insertSizedBoxBetween(height: 4),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
